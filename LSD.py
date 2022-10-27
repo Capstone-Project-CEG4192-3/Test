c452e638b3a885036ae8165b7c46afa87baea87d
mdakk072 <71794329+mdakk072@users.noreply.github.com>
2022-10-27 16:56:16 -0400

    Add files via upload

--- LSD.py
+++ LSD.py
@@ -12,6 +12,7 @@ spaces that we could shape with the time ( cars parking in )
 
 '''
 from audioop import cross
+from cmath import cos
 import copy
 import math
 import time
@@ -23,15 +24,13 @@ import math
 import keyboard
 import ast
 import random
-#store the y=mx+b lines
-bigLines=[]
 
 #func to get intersection btwn to lines line1((x0 y0) x1 y1) line2((x0 y0) (x1 y1))
 def line_intersection(line1, line2):
     #acces to space matrix (which is the matrix that tells us if a pixel in the picture is worth to compute or no )
     global space
 
-    #intersection detect (https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines)
+    #intersection detect : source  (https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines)
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
@@ -39,6 +38,7 @@ def line_intersection(line1, line2):
         return a[0] * b[1] - a[1] * b[0]
 
     div = det(xdiff, ydiff)
+    #avoid infinite division
     if div == 0:
        return False
 
@@ -63,27 +63,20 @@ def line_intersection(line1, line2):
 
 ######################################### MAIN #############################################
 
-#all files
-files = ['parking.jpg','parking2.jpg','p3.jpg','p4.jpg','input.jpg','outin.webp','int.webp','inputTest.png','input1.webp']
-#files to test
-demo=['parking2.jpg','inputTest.png','input.jpg','input1.webp','outin.webp',]
-random.shuffle(files)
-#if you want random demo order
-random.shuffle(demo)
 
+def detectLines(file):
+    #store the y=mx+b lines
 
+    bigLines=[]
 
-
-for po,file in enumerate(demo):
     #we read the frame/picture
-    file='outin.webp'
     img = cv2.imread(file)
     #filter
     gr=cv2.cvtColor(src=img, code=cv2.COLOR_BGR2GRAY)
     #filter to lower details
     blur_gray = cv2.GaussianBlur(src=gr, ksize=(5, 5), sigmaX=0)
     #threshold used for canny edges detection
-    thresholds=[40, 180,]
+    thresholds=[30, 200,]
     #read picture/frame
     img = cv2.imread(file)   
     #state str
@@ -126,9 +119,10 @@ for po,file in enumerate(demo):
         #   if same x absis slope is vertical :)  det=0 
         if line1[1][0]!=line1[0][0]:
             det = (line1[1][1]-line1[0][1])/(line1[1][0]-line1[0][0])
-        else:det=9
+        else:det=0
         #update title with plotting line number
-        plt.title(f'[{po+1}/{len(files)}]{state} Line {ff+1}/{len(lines)}')
+        #plt.title(f'[{po+1}/{len(files)}]{state} Line {ff+1}/{len(lines)}')
+        plt.title(f'Low Detail image')
         #Plot detected line Instruction :
         #plt.plot([line1[0][0],line1[1][0]],[line1[0][1],line1[1][1]],color='r',linewidth=1)
         #create list of number from 0 to picture width (x)
@@ -139,13 +133,30 @@ for po,file in enumerate(demo):
         y0= det*(0-line1[1][0])+line1[1][1]
         #new x y list to filter points
         nx=[]
+        dx=[]
         ny=[]
+        dy=[]
         #we check our line by iterating the  points
         for s in range(len(x)):
             #skip if point is not on picture size range (0-i for y) (0-j for x but already satisfafied)
-            if  (int(y[s])>=i or y[s]<0) or (int(x[s])>=j or x[s]<0) :continue
+            if  (int(y[s])>=i or int(y[s])<0) or (int(x[s])>=j or x[s]<0) :continue
             #Skip if point is not active according to space matrix
-            if space[int(y[s]),int(x[s])]==0:continue
+            if space[int(y[s]),int(x[s])]==0:
+                
+
+                for u in range(5):
+                    for v in range(5):
+                        try:t= space[int(y[s])+v,int(x[s])+u]
+                        except: t=0
+                        if t>0:
+                            #increment space index to manifest more trust for the point (since the point is on active pixel and is part of a line)
+                            try:space[int(y[s])+v,int(x[s])+u]+=1
+                            except:continue
+                            # add x y to new lists
+                            nx.append(int(x[s])+u)
+                            ny.append(int(y[s])+v)
+                dx.append(x[s])
+                dy.append(y[s])
             #if its active point and in pciture size 
             else:
                 #increment space index to manifest more trust for the point (since the point is on active pixel and is part of a line)
@@ -153,89 +164,62 @@ for po,file in enumerate(demo):
                 # add x y to new lists
                 nx.append(x[s])
                 ny.append(y[s])
-                #list containing our x and y and  the slope our y0 and representation of 4 points of the line
-        equa=[x,y,det,y0,[[nx[0],ny[0]],[nx[-1],ny[-1]]]]
-        #add line to our lines list
-        bigLines.append(equa)
-        #check intersection between saved lines (or we can use a function to detect if two lines are similar and save just one of the two)
-        comp=comp+[line_intersection(lst1[-1],lst2[-1]) for lst1 in bigLines for lst2 in bigLines if line_intersection(lst1[-1],lst2[-1])!=False]
-        #draw img
-        img2 = cv2.imread(file)   
-        #draw filtred line in opencv
-        cv2.line(img2, [int(nx[0]),int(ny[0])], [int(nx[-1]),int(ny[-1])], (0, 0, 255), 2)
-        plt.pause(0.8)
-        cv2.imshow("LSD",img2 )
-          
-        print(f'> Line  {ff+1}/{len(lines)} : y0: {equa[-2]} slope: {det}  equa : y={det:.2}x+{det+y0:.2}')
-        print(f'>Intersection points before set  : ',(len(comp)))
-        if len(comp)>=1:
-            comp=[ (int(xx),int(y)) for xx,y in comp]
-            comp=set(comp)
-            comp=list(comp)
-            print(f'>Intersection points after set  : ',(len(comp)))
-        #sort list of intersection according to : 
-        #distance from origin
-        comp.sort(key=lambda x:math.sqrt(x[0]**2+x[1]**2))
-        #ratio distance/angle
-        #comp.sort(key=lambda x:math.sqrt(x[0]**2+x[1]**2)/(math.atan(x[1]/x[0])))
-        #actual point angle with origin
-        ang1=0
-        #previous point angle with origin
-        ang2=0
-        #actual distance btwn point and origin 
-        ccs1=0
-        #previous distance btwn point and origin 
-        ccs2=0
-        #previous point
-        prevP=[0,0]
-        #savedpoints
-        accumPointList=[]
-        #if there is intersection points
-        if len(comp)>=1:
-            #vvv is index and ccs is actual point in the list of intersection points
-            for vvv ,ccs in enumerate(comp):
-                #we get distance and angle
-                ccs1=math.sqrt(ccs[0]**2+ccs[1]**2)
-                ang1=math.atan(ccs[1]/ccs[0])*180/math.pi
-                #print actual point infos
-                print(f'> Intersect Point  {vvv+1}/{len(comp)} :  ',ccs,f'   -- distance: {ccs1}  -- angle : {ang1}')
-                #IF Statement to tell if actual point is the same as previois point as the list of points is sorted (testing it to see if it works)
-                #we check distance btwn points + angle difference and x and y differences 
-                #we can test cross product if 2 points are on same postion matbe cross product close to 0
-                print(f'>cross : {np.cross(ccs,prevP)}')
-                if abs(ccs1-ccs2)<20 and  abs(ang1-ang2)<10 and abs(ccs[0]-prevP[0])<10 and abs(ccs[1]-prevP[1])<10 :
-                    #if two close points add then to the list accumulator 
-                    accumPointList.append(ccs)
-                    #if the actual point and previous point are not close we draw the actual saved points as one point since they are all similar 
-                else:
-                    #check if we have a lot of points that spot the same point position (generaly >2 or >5)
-                    if len(accumPointList)>2:
-                        #SOME Np results to compare
-                        average=np.average(accumPointList,axis=0)
-                        mediane=np.median(accumPointList,axis=0)
-                        mean=np.mean(accumPointList,axis=0)
-                        #using median for now
-                        graal=mediane
-                        print('>$$$$$$$ Average   :', average)
-                        print('>$$$$$$$ Mediane   :', mediane)
-                        print('>$$$$$$$ Mean   :', mean)
-                        print('>$$$$$$$ Used   :', graal)
-                        print('>$$$$$$$ SPACEMatrix at this position :' ,{space[int(graal[1]),int(graal[0])]})
-                        #maybe we can use this alsp : 
-                        print(f'> %%%%   %%%% Result  {len(accumPointList)}/{len(comp)} Points pointing to => {graal}')
-                        #we draw the point in matplotlib
-                        plt.scatter(int(graal[0]),int(graal[1]))
-                        #reset acumulator list for a new point
-                        accumPointList=[]
-                        #split line for next Point to draw
-                        print('-----------------------------------')
-                        #plt.pause(0.5)
-                #we save actual point info
-                prevP=ccs
-                ccs2=math.sqrt(ccs[0]**2+ccs[1]**2)
-                ang2=math.atan(ccs[1]/ccs[0])*180/math.pi
+                dx.append(x[s])
+                dy.append(y[s])
 
+        #list containing our x and y and  the slope our y0 and representation of 4 points of the line
+        equa=[nx,ny,det,y0,[[dx[0],dy[0]],[dx[-1],dy[-1]]]]
+        #if fist iteration add line
+        if len(bigLines)==0:
+            bigLines.append(equa)
+            continue
+        #we suppose a new line
+        newLine=True
+        #we iterate trought saved lines
+        #A
+        ll2=equa[-1]
+        l1=(ll2[1][0]-ll2[0][0],ll2[1][1]-ll2[0][1])
+        for bv,ll in enumerate(bigLines):
+            #loop line
+            ll=ll[-1]
+            l2=(ll[1][0]-ll[0][0],ll[1][1]-ll[0][1])
+            costeta=(np.dot(l1,l2))/(math.sqrt(l2[0]**2+l2[1]**2)*(math.sqrt(l1[0]**2+l1[1]**2)))
+            c=np.cross(l1,l2)
+            stateee=''
+            if np.arccos(costeta)*180/math.pi<15:
+                stateee='Parallal'
+            if 70<np.arccos(costeta)*180/math.pi<97:
+                stateee='Perpendicular'
+
+            diffs=[abs(ll2[i][j]-ll[i][j]) for i in range(2) for j in range(2)]
         
-            if cv2.waitKey(1) & 0xFF == ord('q'):
+            print(f'>>>>>Line {ff}--{bv}    \n      COS={costeta:.2}\n      angle: {(np.arccos(costeta)*180/math.pi)}\n     crossP: {c:.4} ')
+            diffxy=30
+            print(f'        points  <10   :{all(x<diffxy for x in diffs)}')
+            print(f'*******End Line {ff}--{bv}   ******** ')
+            if all(x<diffxy for x in diffs) and (costeta>0.78):
+                newLine=False
+                print(f'>Same line : Line {ff}--{bv}')
+
                 break
+        if newLine:
+            bigLines.append(equa)    
+           
+    print(f'>  Result :  {len(bigLines)} Lines')
+    for kj,las in enumerate(bigLines):
        
+        print(f'> Line {kj}/{len(bigLines)-1}  : {len(las[0])}')
+        plt.scatter(las[0],las[1],linewidths=0.5)
+        plt.pause(0.1)
+    plt.title('space')
+    plt.show()
+
+
+#all files to test ( must be pictores of parking space with min number of cars empty if possible for now)
+files = ['parking.jpg','parking2.jpg','input.jpg','outin.webp','int.webp','inputTest.png','input1.webp']
+#files to test
+random.shuffle(files)
+#if you want random demo order
+
+for po,file in enumerate(files):
+    detectLines(file)
